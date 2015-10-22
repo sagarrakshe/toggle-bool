8c68238ffbe0bd8962a82879c0bb9c2ca5b5bce9
sagarrakshe <sagarrakshe2@gmail.com>
2015-10-22 14:40:33 +0530

    Pep8 Code formatting

--- plugin/toggle_bool.py
+++ plugin/toggle_bool.py
@@ -1,29 +1,28 @@
 values = {
-        'true': 'false',
+    'true': 'false',
+    'on': 'off',
+    'yes': 'no',
+    '1': '0',
+}
 
-        'on': 'off',
-
-        'yes': 'no',
-
-        '1': '0',
-        }
 
 def format_value(word, toggleWord):
     if word.isupper():
-      toggleWord = toggleWord.upper()
+        toggleWord = toggleWord.upper()
     elif word.istitle():
-      toggleWord = toggleWord.title()
+        toggleWord = toggleWord.title()
     return toggleWord
+
 
 def toggle_bool_value(word):
     toggleWord = word
     for key in values.keys():
-      if key == word.lower():
-        toggleWord = values[key]
-        toggleWord = format_value(word, toggleWord)
-        break
-      elif values[key] == word.lower():
-        toggleWord = key
-        toggleWord = format_value(word, toggleWord)
-        break
+        if key == word.lower():
+            toggleWord = values[key]
+            toggleWord = format_value(word, toggleWord)
+            break
+        elif values[key] == word.lower():
+            toggleWord = key
+            toggleWord = format_value(word, toggleWord)
+            break
     return toggleWord
